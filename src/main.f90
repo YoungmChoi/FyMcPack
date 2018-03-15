633015476ddaec1a71427ae2f133589f00463c50
ychoi <youngmyung.choi@ec-nantes.fr>
2018-03-15 13:10:15 +0100

    Add License.
    Signed-off-by: ychoi <youngmyung.choi@ec-nantes.fr>

--- src/main.f90
+++ src/main.f90
@@ -1,28 +1,78 @@
-    Program main
+!!*------------------------------------------------------------------*!!
+!!  FyMc Pack
+!!*------------------------------------------------------------------*!!
+!!  BSD 3-Clause License
+!!
+!!  Copyright (c) 2018, YoungMyung Choi
+!!  All rights reserved.
+!!
+!!  Redistribution and use in source and binary forms, with or without
+!!  modification, are permitted provided that the following conditions are met:
+!!
+!!  * Redistributions of source code must retain the above copyright notice, this
+!!    list of conditions and the following disclaimer.
+!!
+!!  * Redistributions in binary form must reproduce the above copyright notice,
+!!    this list of conditions and the following disclaimer in the documentation
+!!    and/or other materials provided with the distribution.
+!!
+!!  * Neither the name of the copyright holder nor the names of its
+!!    contributors may be used to endorse or promote products derived from
+!!    this software without specific prior written permission.
+!!
+!!  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+!!  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+!!  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+!!  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+!!  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+!!  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+!!  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+!!  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+!!  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+!!  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!!
+!!*------------------------------------------------------------------*!!
+!!
+!!  Fortran Program
+!!      main
+!!
+!!  Authors
+!!      YoungMyung Choi, Ecole Centrale de Nantes
+!!          - youngmyung.choi@ec-nantes.fr
+!!
+!!*------------------------------------------------------------------*!!
 
-        Use mfpGlobal
-        use mfpGeneral
-        use mfpGeometry
+!! -----------------------------------------------------------------------------
+Program main
+!! -----------------------------------------------------------------------------
 
-    Implicit None
+    Use mfpGlobal
+    use mfpGeneral
+    use mfpGeometry
 
-        integer :: i
+!! -----------------------------------------------------------------------------
 
-        !!... Test General
+Implicit None
 
-            ! Call testString()
+    integer :: i
 
-            Call testDictionary
+    !!... Test General
 
-            ! Call testSLTable()
+        ! Call testString()
 
-            ! Call testLLink()
+        Call testDictionary
 
-            ! Call testDataArray()
+        ! Call testSLTable()
+
+        ! Call testLLink()
+
+        ! Call testDataArray()
 
 
-        !!... Test Geometry
+    !!... Test Geometry
 
-            ! Call testSurfMesh()
+        ! Call testSurfMesh()
 
-    End Program
+End Program
+
+!! -----------------------------------------------------------------------------
